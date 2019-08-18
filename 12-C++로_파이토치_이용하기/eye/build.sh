262b465dd44ac1e49f1abdf8e6808c4957713bf8
keon <kwk236@gmail.com>
2019-08-17 22:24:30 -0700

    remove chapter 11, 12

--- 12-C++로_파이토치_이용하기/eye/build.sh
+++
@@ -1,5 +0,0 @@
-mkdir build
-cd build
-cmake -DCMAKE_PREFIX_PATH=$LIBPYTORCH_PATH ..
-cd ..
-make
--- 12-C++로_파이토치_이용하기/eye/eye.cpp
+++
@@ -1,8 +0,0 @@
-#include <torch/torch.h>
-#include <iostream>
-
-int main() {
-  torch::Tensor tensor = torch::eye(3);
-  std::cout << tensor << std::endl;
-}
-
